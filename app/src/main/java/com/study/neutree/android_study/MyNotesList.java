83f9f1722962251c7bb211d4ae8d30cfcf031759
Neutree <czd666666@gmail.com>
2015-11-13 20:28:38 +0800

    use of simple adapter

--- app/src/main/java/com/study/neutree/android_study/MyNotesList.java
+++ app/src/main/java/com/study/neutree/android_study/MyNotesList.java
@@ -8,6 +8,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.Toast;
 
 import com.study.neutree.adapter.NotesListItem;
@@ -15,7 +16,10 @@ import com.study.neutree.model.NoteList;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 
 public class MyNotesList extends AppCompatActivity {
@@ -43,7 +47,32 @@ public class MyNotesList extends AppCompatActivity {
                 getResources().getString(R.string.LastEditDateTime) + getResources().getString(R.string.colon) + "2015-10-28 10:10"));
 
         NotesListItem adapter=new NotesListItem(this,listForum);
-        listView.setAdapter(adapter);
+
+        //simpleadapter的使用
+        List<Map<String,Object>> mapList = new ArrayList<Map<String,Object>>();
+        Map<String,Object> data1=new HashMap<String,Object>();
+        data1.put("image",R.drawable.head_pic);
+        data1.put("title","啊哈哈哈哈哈");
+        data1.put("user", "neucrack");
+        data1.put("love", "100");
+        data1.put("date", "2015-11-13");
+        mapList.add(data1);
+
+        Map<String,Object> data2=new HashMap<String,Object>();
+        data2.put("image",R.drawable.head_pic);
+        data2.put("title","啊啊啊啊啊啊啊啊");
+        data2.put("user", "neucrack");
+        data2.put("love", "50000");
+        data2.put("date", "2015-11-13");
+        mapList.add(data2);
+
+        //simpleAdapter 的好处就是简单，缺点就是只能为整个item添加点击事件，自定义的item还可以为item里面的控件添加事件
+        SimpleAdapter adapter2=new SimpleAdapter(this,
+                mapList,R.layout.note_list_item,new String[]{"image","title","user","love","date"},
+                new int[]{R.id.noteListItemImage,R.id.noteListItemUser,R.id.noteListItemLoveNum,
+                        R.id.noteListItemLastEditTime});
+        listView.setAdapter(adapter2);//应用 simpleadapter
+       // listView.setAdapter(adapter);//应用自定义的adapter
         //添加点击监听时间
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
